1d30f454eddeb7622fe363611fb24fc74edb2896
Jess V <valia010@umn.edu>
2024-05-08 16:15:24 -0500

    Create varying-resistance-simulation.R

---
+++ varying-resistance-simulation.R
@@ -0,0 +1,23 @@
+# load libraries 
+#install.packages(pacman)
+pacman::p_load(deSolve,ggplot2,plyr)
+
+# initiate state variables: host population abundances
+init <- c(
+  S=500, # susceptible subpopulation
+  I1=100, #infected subpop by virulence class 1
+  I2=100, #infected subpop by virulence class 2
+  I3=100, #infected subpop by virulence class 3
+  I4=100, #infected subpop by virulence class 4
+  I5=100, #infected subpop by virulence class 5
+  I6=100, #infected subpop by virulence class 6
+  I7=100, #infected subpop by virulence class 7
+  I8=100, #infected subpop by virulence class 8
+  I9=100, #infected subpop by virulence class 9
+  I10=100 #infected subpop by virulence class 10
+)
+
+# initate time of simulation 
+times <- seq(0, 500, by = 1)
+
+# simulation 1: vary exclusively host resistance
