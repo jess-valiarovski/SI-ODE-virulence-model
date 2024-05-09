1c7eef6e2f066d492cda0d6bafa32f21f4a8f2e3
Jess V <valia010@umn.edu>
2024-05-09 13:37:18 -0500

    Update varying-resistance-simulation.R

--- varying-resistance-simulation.R
+++ varying-resistance-simulation.R
@@ -20,8 +20,7 @@ init <- c(
 # initate time of simulation 
 times <- seq(0, 500, by = 1)
 
-# simulation 1: vary exclusively host resistance
-
+# vary exclusively host resistance 
 parameters_resis <- expand.grid(m_t = 0,
                            m_r = seq(0,1,0.05),
                            beta = 0.4,
@@ -29,25 +28,24 @@ parameters_resis <- expand.grid(m_t = 0,
                            miu_s = 0.1,
                            fecundity = 1
 )
-#Simulation ODE
 
+#Simulation ODE
 SIM_MODEL <- function(times, init, parameters) {
   with(as.list(c(init, parameters)),{
-  
-  # extract state variables from init vector
+    
+    # extract state variables from init vector
     S = as.vector(init[1]) # susceptible host subpop
     I = matrix(init[2:length(init)], nrow=10, ncol=1) # infected host subpop
     
-   # can it run without this? below b/c you have it already defined globally
-  # modified parameters i.e. "realized" rates   
-  #  beta_realized = matrix(data=NA, nrow=10,ncol=1) # realized transmission rate
-  #  alpha = seq(0.1,1,0.1) # virulence class
-  #  for (z in 1:10){
-  #    beta_realized[z] <- alpha[z]*beta*(1-m_r) # realized transmission rate
-  #  }
-   # miu_i = (sqrt(miu_s)+ (1-m_t)*(beta_realized/beta))**2 # realized mortality rate
+    # modified parameters i.e. "realized" rates   
+    beta_realized = matrix(data=NA, nrow=10,ncol=1) # realized transmission rate
+    alpha = seq(0.1,1,0.1) # virulence class
+    for (z in 1:10){
+      beta_realized[z] <- alpha[z]*beta*(1-m_r) # realized transmission rate
+    }
+    miu_i = (sqrt(miu_s)+ (1-m_t)*(beta_realized/beta))**2 # realized mortality rate
     
-  # system of differential equations for SI model
+    # system of differential equations for SI model
     dS <- -1*S*sum(I*beta_realized) - miu_s * S + sum(gamma * I) + fecundity * (S+sum(I))
     dI <- I * beta_realized * S - (gamma+miu_i) * I
     return(list(c(dS,dI)))
@@ -55,10 +53,9 @@ SIM_MODEL <- function(times, init, parameters) {
 }
 rootfun <- function(times, init, parameters) {
   dstate <- unlist(SIM_MODEL(times, init, parameters))
-  sum(abs(dstate)) - 1e-1 #this function isn't working...
+  sum(abs(dstate)) - 1e-1 
 }
-
-# run simulation!
+  
 for (i in 1:nrow(parameters_resis)){
   out <- lsodar(func = SIM_MODEL, y = init, parms = parameters_resis[i,],
                 times = times, rootfun = rootfun)
@@ -68,5 +65,4 @@ for (i in 1:nrow(parameters_resis)){
   }
   parameters_resis[i,'avg_class'] = out_df[nrow(out_df),length(out_df)]
 }
-# generate figure
 ggplot(parameters_resis, aes(x=m_r,y=avg_class)) + geom_point() + stat_smooth(method = "loess")
